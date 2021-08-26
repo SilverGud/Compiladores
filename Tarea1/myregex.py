1c4065a18e4bf4590a6bb86132a472c7576ff559
Miguel Alejandro Hernandez Garcia <a01654722@itesm.mx>
2021-08-25 21:41:32 -0500

    tarea1

---
+++ Tarea1/myregex.py
@@ -0,0 +1,16 @@
+import re
+
+
+def email(mail):
+    correoExpresion = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+    return re.match(correoExpresion, mail) is not None
+
+
+def phone_number(numero):
+    numeroExpresion = r"^(\(?\+[\d]{1,3}\)?)\s?([\d]{1,5})\s?([\d][\s\.-]?){6,7}$"
+    return re.match(numeroExpresion, numero) is not None
+
+
+def hexadecimal(hexa):
+    hexaExpresion = r"^[0-9A-F]+$"
+    return re.match(hexaExpresion, hexa) is not None
